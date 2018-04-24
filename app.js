fd00c394d165327079bf59322bb3441812f299cb
Alan Yan <acyanlight@gmail.com>
2018-04-24 11:03:46 -0400

    history updates upon nav change

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const app = express();
+
 const bodyParser = require('body-parser')
 const path = require('path');
 const logger = require('morgan');
 const child_process = require('child_process');
 const fs = require('fs');
-
-const historicalData = require('./data.json');
-
-const app = express();
 
 // handle static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,8 +18,10 @@ app.get('/', (req, res) => res.sendFile('index.html', { root: path.join(__dirnam
 app.get('/history', (req, res) => res.sendFile('history.html', { root: path.join(__dirname, '') }));
 
 app.get('/getHistoricalData', (req, res) => {
-  console.log(historicalData)
-  res.json(historicalData);
+  purgeCache('./data.json');
+  let historicalData = require('./data.json')
+  res.json(historicalData)
+  // console.log(historicalData)
 });
 
 app.post('/analyze', (req, res) => {
@@ -31,7 +31,6 @@ app.post('/analyze', (req, res) => {
   // spawn child_process to run topic modeling script
   let spawn = child_process.spawn;
   var py  = spawn('python3', ['topic_modelling/compute_input.py']);
-  // var data = [1,2,3,4,5,6,7,8,9];
   var dataString = '';
 
   py.stdout.on('data', function(response){
@@ -78,3 +77,49 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8000, () => console.log('App listening on port 8000...'))
+
+// Some helper functions
+/**
+ * Removes a module from the cache
+ */
+function purgeCache(moduleName) {
+    // Traverse the cache looking for the files
+    // loaded by the specified module name
+    searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+        if (cacheKey.indexOf(moduleName)>0) {
+            delete module.constructor._pathCache[cacheKey];
+        }
+    });
+};
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function traverse(mod) {
+            // Go over each of the module's children and
+            // traverse them
+            mod.children.forEach(function (child) {
+                traverse(child);
+            });
+
+            // Call the specified callback providing the
+            // found cached module
+            callback(mod);
+        }(mod));
+    }
+};
