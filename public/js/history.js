594a6ab2eb1050977df1d5c6588031ee6dd59b5f
Alan Yan <acyanlight@gmail.com>
2018-05-06 17:25:42 -0400

    add keyword graph to history view - does not toggle yet

--- public/js/history.js
+++ public/js/history.js
@@ -28,7 +28,7 @@ function loadData() {
       let feed = document.getElementById('history-feed');
       historicalDataArr.forEach((obj) => {
         let col = document.createElement('div');
-        col.className = 'col-sm-6';
+        col.className = 'col-sm-12';
         feed.appendChild(col);
 
         let card = document.createElement('div');
@@ -53,7 +53,10 @@ function loadData() {
         obj['topics'].forEach((topic) => {
           let cardTopic = document.createElement('button');
           cardTopic.className = 'btn btn-primary';
-          // cardTopic.style.color = 'black';
+
+          cardTopic.addEventListener("click", function() {
+            toggleKeywordGraph(cardBody, obj['keywords']);
+          });
           cardTopic.innerHTML = topic;
           cardBody.appendChild(cardTopic);
           if (!topics_arr.indexOf(topic) > -1) {
@@ -63,7 +66,10 @@ function loadData() {
           card.id = topic;
         });
 
+        let linebreak = document.createElement("br");
+        cardBody.appendChild(linebreak);
       });
+
       // load initial navbar
       topics_arr.forEach((topic) => {
         let filterButton = document.createElement('button');
@@ -75,4 +81,84 @@ function loadData() {
         navbar_element.appendChild(filterButton);
       })
   });
+}
+
+// function to toggle keyword bar chart of selected topic
+function toggleKeywordGraph(cardBody, keywordArr) {
+  // set the dimensions of the canvas
+  var margin = {top: 20, right: 20, bottom: 70, left: 52},
+      width = 600 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  // set the ranges
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  // define the axis
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+
+  // add the SVG element
+  var svg = d3.select(cardBody).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // load the data
+  let keywordObj = keywordArr[0];
+  let dataArray = [];
+
+  for (let keyword in keywordObj) {
+    dataArray.push(
+      {
+        "Keyword": keyword,
+        "Strength": keywordObj[keyword]
+      }
+    )
+  }
+
+  // scale the range of the data
+  x.domain(dataArray.map(function(d) { return d.Keyword; }));
+  y.domain([0, d3.max(dataArray, function(d) { return d.Strength; })]);
+
+  // add axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" )
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", ".71em")
+      .style("text-anchor", "middle")
+      .text("Keyword Strength");
+
+  // Add bar chart
+  svg.selectAll("bar")
+      .data(dataArray)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.Keyword); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.Strength); })
+      .attr("height", function(d) { return height - y(d.Strength); });
 }
