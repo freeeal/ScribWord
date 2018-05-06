671aa2ef2b9fa4587f411ba4079cc129e0be397f
Alan Yan <acyanlight@gmail.com>
2018-05-06 16:25:33 -0400

    add filter navbar

--- public/js/history.js
+++ public/js/history.js
@@ -1,3 +1,23 @@
+const navbar_element = document.getElementById('filter-navbar');
+let topics_arr = [];
+
+function reloadFilteredTopics(topic) {
+  let all_card_elements = document.getElementsByClassName('card');
+  console.log(all_card_elements)
+  for (i = 0; i < all_card_elements.length; i++) {
+    let thisEle = all_card_elements[i];
+    if (topic !== 'all') {
+      if (thisEle.id === topic) {
+        thisEle.style.display = "block";
+      } else {
+        thisEle.style.display = "none";
+      }
+    } else {
+      thisEle.style.display = "block";
+    }
+  }
+}
+
 function loadData() {
   let historicalData;
   fetch('/getHistoricalData')
@@ -12,7 +32,7 @@ function loadData() {
         feed.appendChild(col);
 
         let card = document.createElement('div');
-        card.className = 'card';
+        card.className = 'card bg-light';
         col.appendChild(card);
 
         let cardBody = document.createElement('div');
@@ -31,12 +51,28 @@ function loadData() {
 
         // append topics
         obj['topics'].forEach((topic) => {
-          let cardTopic = document.createElement('a');
+          let cardTopic = document.createElement('button');
           cardTopic.className = 'btn btn-primary';
+          // cardTopic.style.color = 'black';
           cardTopic.innerHTML = topic;
-          cardBody.appendChild(cardTopic)
+          cardBody.appendChild(cardTopic);
+          if (!topics_arr.indexOf(topic) > -1) {
+            topics_arr.push(topic);
+          }
+          // set card id to topic -- for filtering
+          card.id = topic;
         });
 
       });
-    });
+      // load initial navbar
+      topics_arr.forEach((topic) => {
+        let filterButton = document.createElement('button');
+        filterButton.className = "btn btn-primary";
+        filterButton.innerHTML = topic;
+        filterButton.addEventListener("click", function() {
+          reloadFilteredTopics(topic);
+        });
+        navbar_element.appendChild(filterButton);
+      })
+  });
 }
