355a89b4ad4f3d1d8d1ff9be661d66d5d1ac3702
Alan Yan <acyanlight@gmail.com>
2018-05-13 21:38:14 -0400

    add compare topics graph to history page

--- public/js/history.js
+++ public/js/history.js
@@ -67,11 +67,10 @@ function loadData() {
         obj['topTopics'].forEach((topic, i) => {
           let cardTopic = document.createElement('button');
           cardTopic.className = 'btn btn-primary';
-
+          cardTopic.innerHTML = topic;
           cardTopic.addEventListener("click", function() {
             toggleKeywordGraph(cardBody, obj['keywords'][i]);
           });
-          cardTopic.innerHTML = topic;
 
           cardBody.appendChild(cardTopic);
           if (!topics_arr.includes(topic)) {
@@ -82,8 +81,15 @@ function loadData() {
           card.id += topic + ' ';
         });
 
-        let linebreak = document.createElement("br");
-        cardBody.appendChild(linebreak);
+        let compareTopicButton = document.createElement('copyButton');
+        compareTopicButton.className = 'btn btn-info';
+        compareTopicButton.innerHTML = 'Compare Topics';
+        compareTopicButton.setAttribute('style', 'float:right');
+        compareTopicButton.addEventListener("click", function() {
+          toggleCompareGraph(cardBody, obj['allTopics'], obj['likelihoods']);
+        });
+
+        cardBody.appendChild(compareTopicButton);
       });
 
       // load initial navbar
@@ -123,7 +129,6 @@ function toggleKeywordGraph(cardBody, keywordObj) {
       .orient("left")
       .ticks(10);
 
-  // console.log(cardBody.childNodes)
   // toggle functionality
   let svgExists = false;
   for (i = 0; i < cardBody.childNodes.length; i++) {
@@ -136,6 +141,7 @@ function toggleKeywordGraph(cardBody, keywordObj) {
   if (!svgExists) {
     // add the SVG element
     var svg = d3.select(cardBody).append("svg")
+        .attr("id", "keyword")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -190,8 +196,110 @@ function toggleKeywordGraph(cardBody, keywordObj) {
         .attr("y", function(d) { return y(d.Strength); })
         .attr("height", function(d) { return height - y(d.Strength); });
   } else {
+    console.log(cardBody.lastChild);
     cardBody.removeChild(cardBody.lastChild);
     toggleKeywordGraph(cardBody, keywordObj)
+  }
+
+}
+
+// function to toggle keyword bar chart of selected topic
+function toggleCompareGraph(cardBody, allTopics, allLikelihoods) {
+  console.log(allTopics);
+  console.log(allLikelihoods);
+  // set the dimensions of the canvas
+  var margin = {top: 20, right: 20, bottom: 70, left: 52},
+      width = 600 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  // set the ranges
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  // define the axis
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+
+  // toggle functionality
+  let svgExists = false;
+  for (i = 0; i < cardBody.childNodes.length; i++) {
+    let childNode = cardBody.childNodes[i];
+    if (childNode.nodeName === "svg") {
+      svgExists = true;
+    }
+  }
+
+  if (!svgExists) {
+    // add the SVG element
+    var svg = d3.select(cardBody).append("svg")
+        .attr("id", "compare")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    // load the data
+    let topics = allTopics;
+    let likelihoods = allLikelihoods;
+    let dataArray = [];
+
+    for (let i=0; i<topics.length; i++) {
+      dataArray.push(
+        {
+          "Topic": topics[i],
+          "Likelihood": likelihoods[i]
+        }
+      )
+    }
+
+    // scale the range of the data
+    x.domain(dataArray.map(function(d) { return d.Topic; }));
+    y.domain([0, d3.max(dataArray, function(d) { return d.Likelihood; })]);
+
+    // add axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)" )
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", ".71em")
+        .style("text-anchor", "middle")
+        .text("Topic Likelihood");
+
+    // Add bar chart
+    svg.selectAll("bar")
+        .data(dataArray)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.Topic); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.Likelihood); })
+        .attr("height", function(d) { return height - y(d.Likelihood); });
+  } else {
+    console.log(cardBody.lastChild);
+    console.log(cardBody.lastChild.id)
+    cardBody.removeChild(cardBody.lastChild);
+    toggleCompareGraph(cardBody, allTopics, allLikelihoods);
   }
 
 }
